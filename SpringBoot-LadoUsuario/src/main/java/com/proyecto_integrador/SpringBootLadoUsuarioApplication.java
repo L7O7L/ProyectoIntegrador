d5c7d2a213205b702213187328aa7b12dc426f4f
L7O7L <luisbaltazarsegura@gmail.com>
2022-11-01 16:59:22 -0500

    Commit 2

---
+++ SpringBoot-LadoUsuario/src/main/java/com/proyecto_integrador/SpringBootLadoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto_integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLadoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLadoUsuarioApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-LadoUsuario/src/test/java/com/proyecto_integrador/SpringBootLadoUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto_integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLadoUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
